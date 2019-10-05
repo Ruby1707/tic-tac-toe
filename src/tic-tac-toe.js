21f368905043bceb6414a0b891e86bbb38f72ade
Ruby1707 <53220616+Ruby1707@users.noreply.github.com>
2019-10-05 16:27:47 +0300

    Update tic-tac-toe.js

--- src/tic-tac-toe.js
+++ src/tic-tac-toe.js
@@ -1,89 +1,88 @@
-class FSM {
-    constructor(config) {
-        this.configuration=config;
-        this.newState=this.configuration.initial;
-        this.history=[];
-        this.point=this.configuration.states.length
-        this.states = Object.keys(config.states);
-        this.prevState = null;
-        this.count = 0;
-        this.history.push(this.newState);
-        this.canRedo = false;
+class TicTacToe {
+    constructor() {
+      this.gamefield = [[null, null, null], [null, null, null], [null, null, null]];
+      this.play = 'x';
+    }
+    getCurrentPlayerSymbol() {
+      return this.play;
+    }
+    nextTurn(rowIndex, columnIndex) {
+      if (this.gamefield[rowIndex][columnIndex] == null) {
+            this.gamefield[rowIndex][columnIndex] = this.play
+
+            if (this.play == 'x'){
+              this.play = 'o'
+            }
+            else{
+              this.play = 'x'
+            }
+        }
+    }
+    Gg() {
+        let tempArr = [[0,0,0],[0,0,0],[0,0,0]]
+        for (let i = 0; i < 3; i++) {
+            for (let j = 0; j < 3; j++) {
+                if (this.gamefield[i][j] == 'x') tempArr[i][j] = 1
+                if (this.gamefield[i][j] == 'o') tempArr[i][j] = -1
+            }
+        }
+        let sRow = [0,0,0]
+        let sCol = [0,0,0]
+        for (let i = 0; i < 3; i++) {
+            for (let j = 0; j < 3; j++) {
+                sRow[i] = sRow[i] + tempArr[i][j]
+                sCol[i] = sCol[i] + tempArr[j][i]
+            }
+
+            if (Math.abs(sRow[i]) == 3 || Math.abs(sCol[i]) == 3) return true
+        }
+
+        let sDiag = [0,0]
+        for (let i = 0; i < 3; i++) {
+            sDiag[0] = sDiag[0] + tempArr[i][i]
+            sDiag[1] = sDiag[1] + tempArr[i][2-i]
+        }
+
+        if (Math.abs(sDiag[0]) == 3 || Math.abs(sDiag[1]) == 3) return true
+
+        return false
     }
 
-    getState() {
-      return this.newState;
+    isFinished() {
+        if (this.Gg() || this.isDraw()) return true
+        return false
     }
 
-    changeState(state) {
-      if(this.states.indexOf(state)<0){
-        throw new Error();
-      }
-      else{
-        this.prevState = this.newState;
-        this.newState = state;
-      }
-      this.count++;
-      this.history.push(this.newState);
-      this.canRedo = false;
-      this.history.push(state);
+
+    getWinner() {
+      var winner;
+        if (this.play == 'x'){
+          winner= 'o'
+        }
+        if (this.play == 'o'){
+          winner = 'x';
+        }
+        if (this.Gg()){
+          return winner;
+        }
+          return null;
     }
 
-    trigger(event) {
-       if (this.configuration.states[this.newState].transitions[event] === undefined) throw new Error();
-       else{
-         this.prevState = this.newState;
-         this.newState = this.configuration.states[this.newState].transitions[event];
-       }
-       this.history.push(this.newState);
-       this.count++;
-       this.canRedo = false;
-       }
-    reset() {
-      this.newState=this.configuration.initial;
-    }
-    getStates(event) {
-      let arr=[];
-      if(event==undefined){
-        return this.states;
-      }
-      else{
-         for (var key in this.configuration.states) {
-           for (var key1 in this.configuration.states[key].transitions) {
-             if(key1==event){
-               arr.push(key);
-             }
-           }
-         }
-         return arr;
-       }
-    }
-    undo() {
-      this.count++;
-      if (this.count <= 1 ) return false;
-      if (this.newState === this.configuration.initial) return false;
-      const tmp = this.newState;
-      this.newState = this.prevState;
-      this.prevState = tmp;
-      this.canRedo = true;
+    noMoreTurns() {
+      for (let i = 0; i < 3; i++)
+        for (let j = 0; j < 3; j++)
+          if (this.gamefield[i][j] == null) return false
       return true;
     }
-    redo() {
-      if(this.canRedo==false){
+
+    isDraw() {
+      if (this.noMoreTurns() && !this.Gg()) return true;
         return false;
-      }
-      this.count--;
-      if (this.count <= 0) return false;
-      const tmp = this.newState;
-      this.newState = this.prevState;
-      this.prevState = tmp;
-      return true;
     }
-    clearHistory() {
-      this.count=0;
+
+    getFieldValue(rowIndex, colIndex) {
+       return this.gamefield[rowIndex][colIndex];
     }
 }
 
-module.exports = FSM;
-
-
+module.exports = TicTacToe;
